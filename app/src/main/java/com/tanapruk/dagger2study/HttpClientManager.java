2c6963e80ff70f33ad904b0ad78973f0e876d059
Tanapruk <trusthaha@gmail.com>
2016-09-11 15:23:10 +0700

    Init dependencies

---
+++ app/src/main/java/com/tanapruk/dagger2study/HttpClientManager.java
@@ -0,0 +1,12 @@
+package com.tanapruk.dagger2study;
+
+import okhttp3.Cache;
+import okhttp3.OkHttpClient;
+
+/**
+ * Created by trusttanapruk on 9/11/2016.
+ */
+public class HttpClientManager {
+
+
+}
--- app/src/main/java/com/tanapruk/dagger2study/MainActivity.java
+++ app/src/main/java/com/tanapruk/dagger2study/MainActivity.java
@@ -3,11 +3,42 @@ package com.tanapruk.dagger2study;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import okhttp3.Cache;
+import okhttp3.OkHttpClient;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void buildClient() {
+
+
+        int cacheSize = 10 * 1024 * 1024;
+
+        Cache cache = new Cache(getApplication().getCacheDir(), cacheSize);
+
+
+        OkHttpClient client = new OkHttpClient.Builder()
+                .cache(cache)
+                .build();
+
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        GsonConverterFactory converterFactory = GsonConverterFactory.create(gson);
+
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://api.github.com")
+                .addConverterFactory(converterFactory)
+                .client(client)
+                .build();
     }
 }
