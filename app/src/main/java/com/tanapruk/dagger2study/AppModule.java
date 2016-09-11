c041e974532ad76f5039d595e24581d4fe1d34d1
Tanapruk <trusthaha@gmail.com>
2016-09-11 15:54:33 +0700

    add comment to AppModule.class

--- app/src/main/java/com/tanapruk/dagger2study/AppModule.java
+++ app/src/main/java/com/tanapruk/dagger2study/AppModule.java
@@ -11,6 +11,12 @@ import dagger.Provides;
  * Created by trusttanapruk on 9/11/2016.
  */
 
+/**
+ * Annotate with @Module to tell every method that look for injection to come and look inside me
+ * whether this @Provides what it looks for.
+ * In this module it provides {@link Application}
+ * so every other method that need {@link Application} will satisfy its injection from here.
+ */
 @Module
 public class AppModule {
 
@@ -20,6 +26,13 @@ public class AppModule {
         this.mApplication = mApplication;
     }
 
+
+    /**
+     * There are other methods that need {@link Application} so we annotate with @Provides
+     * so those other methods will automatically consume from this provide method
+     * Annotate @Singleton is self explanatory
+     * @return
+     */
     @Provides
     @Singleton
     Application provideApplication() {
